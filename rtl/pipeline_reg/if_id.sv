c7dfbd881d43725107914c2c47b643c7bf506868
Jungle-28 <tranthainhatlam.work@gmail.com>
2025-07-15 00:36:42 +0700

    add assembler, sim

--- rtl/pipeline_reg/if_id.sv
+++ rtl/pipeline_reg/if_id.sv
@@ -1,3 +1,16 @@
-module if_id.sv();
+module if_id(
+	input  logic          clk, rst_n,
+	input  logic [31 : 0] if_instr,
+	output logic [31 : 0] id_instr
+);
+
+	always_ff @(posedge clk, negedge rst_n) begin
+		if(!rst_n) begin
+			id_instr <= '0;
+		end
+		else begin
+			id_instr <= if_instr;
+		end
+	end
 
 endmodule
